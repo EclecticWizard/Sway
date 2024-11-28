bb2ff19f3de3c5b4ecce877c419929caf12b25e5
EclecticWizard <hodgkinsj95@proton.me>
2024-11-28 19:25:25 +0000

    volume indicator

--- status_script.sh
+++ status_script.sh
@@ -2,7 +2,8 @@
 
 while true; do
     media_status=$(playerctl metadata --format "{{ artist }} - {{ title }}" || echo "No media playing")
+    volume=$(pactl list sinks | grep 'Volume:' | awk -F '/' '{print $2}' | head -n 1 | sed 's/^[ \t]*//;s/[ \t]*$//')
     current_time=$(date +"%Y-%m-%d %X")
-    echo "$media_status | $current_time"
-    sleep 1
+    echo "$media_status | 🔊 $volume | $current_time"
+    sleep 0.1
 done
