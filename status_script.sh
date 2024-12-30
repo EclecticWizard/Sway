44f2469265fa1c9c70e3970d800b1420983b8cd1
EclecticWizard <hodgkinsj95@proton.me>
2024-12-30 15:42:54 +0000

    changed to the asla name as ID changed from reboot

--- status_script.sh
+++ status_script.sh
@@ -2,8 +2,8 @@
 
 while true; do
     media_status=$(playerctl metadata --format "{{ artist }} - {{ title }}" || echo "No media playing")
-    speaker_id="61"
-    volumeSpeakers=$(pactl get-sink-volume "$speaker_id" | awk -F '/' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//')
+    speaker_name="alsa_output.pci-0000_26_00.1.hdmi-stereo"
+    volumeSpeakers=$(pactl get-sink-volume "$speaker_name" | awk -F '/' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//')
     current_time=$(date +"%Y-%m-%d %X")
     echo "$media_status | 🔊 $volumeSpeakers | $current_time"
     sleep 0.1
