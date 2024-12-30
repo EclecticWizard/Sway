6e1658b31812a0aef0e9af7d5796b10a22f7f661
EclecticWizard <hodgkinsj95@proton.me>
2024-12-30 08:43:36 +0000

    reworked volume indicator

--- status_script.sh
+++ status_script.sh
@@ -2,8 +2,9 @@
 
 while true; do
     media_status=$(playerctl metadata --format "{{ artist }} - {{ title }}" || echo "No media playing")
-    volume=$(pactl list sinks | grep 'Volume:' | awk -F '/' '{print $2}' | head -n 1 | sed 's/^[ \t]*//;s/[ \t]*$//')
+    speaker_id="61"
+    volumeSpeakers=$(pactl get-sink-volume "$speaker_id" | awk -F '/' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//')
     current_time=$(date +"%Y-%m-%d %X")
-    echo "$media_status | 🔊 $volume | $current_time"
+    echo "$media_status | 🔊 $volumeSpeakers | $current_time"
     sleep 0.1
 done
