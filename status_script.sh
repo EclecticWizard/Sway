be177ad0e763ddb046c81b0ca20b0a871a5d51c8
EclecticWizard <hodgkinsj95@proton.me>
2025-03-15 16:50:03 +0000

    status bar tracks current sink + description

--- status_script.sh
+++ status_script.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 while true; do
-    media_status=$(playerctl metadata --format "{{ artist }} - {{ title }}" || echo "No media playing")
-    speaker_name="alsa_output.pci-0000_26_00.1.hdmi-stereo"
-    volumeSpeakers=$(pactl get-sink-volume "$speaker_name" | awk -F '/' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//')
+    media_status=$(playerctl metadata --format "{{ artist }} - {{title }}" || echo "No media playing")
+    active_sink=$(pactl list short sinks | grep RUNNING | cut -f1)
+        # If no running sink is found, fallback to the default
+    if [ -z "$active_sink" ]; then
+        active_sink=$(pactl info | grep 'Default Sink' | cut -d: -f2 | xargs)
+    fi
+
+        # Get the description of the active sink
+    sink_description=$(pactl list sinks | awk -v sink="$active_sink" '
+         /^Sink #/ {is_target = $2 == "#"sink}
+         /Description:/ && is_target {gsub(/^.*Description: /, ""); print; exit}
+    ')
+
     current_time=$(date +"%Y-%m-%d %X")
-    echo "$media_status | 🔊 $volumeSpeakers | $current_time"
+    volumeSpeakers=$(pactl get-sink-volume "$active_sink" | awk -F '/' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//')
+
+    echo "$media_status | 🔊 $volumeSpeakers $sink_description | $current_time"
     sleep 0.1
 done
